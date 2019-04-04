7c9ecd87273cf75c9a9cf4b1009950d17bad37e2
k8sland <fernand@k8sland.io>
2019-04-04 16:35:50 -0600

    init drop

---
+++ Formula/k9s.rb
@@ -0,0 +1,15 @@
+class Popeye < Formula
+  desc "A Kubernetes Cluster sanitizer and linter"
+  homepage "https://popeye.dev"
+  url "https://github.com/derailed/popeye/releases/download/0.1.0/popeye_0.1.1_Darwin_x86_64.tar.gz"
+  version "0.1.0"
+  sha256 "ad07db979cc0f66bdf0442d6df6eaa58aade347ba0b280b4c6b551fe481d22ae"
+
+  def install
+    bin.install "popeye"
+  end
+
+  test do
+    system "popeye version"
+  end
+end
