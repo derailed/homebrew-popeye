858671b933467973f3ebdab6eea7f528846c9956
k8sland <fernand@k8sland.io>
2019-04-05 00:24:39 -0600

    update formula

--- Formula/popeye.rb
+++ Formula/popeye.rb
@@ -3,7 +3,7 @@ class Popeye < Formula
   homepage "https://imhotep.io/popeye"
   url "https://github.com/derailed/popeye/releases/download/0.1.0/popeye_0.1.0_Darwin_x86_64.tar.gz"
   version "0.1.0"
-  sha256 "ad07db979cc0f66bdf0442d6df6eaa58aade347ba0b280b4c6b551fe481d22ae"
+  sha256 "0c31d893887cbca5bd2824a73a45a8b5914159abb9853f8d10b295e24fdbf224"
 
   def install
     bin.install "popeye"
