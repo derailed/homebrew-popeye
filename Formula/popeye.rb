e84991c68a3286b2c516439330551b1c9ecb19a5
derailed <fernand@imhotep.io>
2019-05-20 23:11:04 -0600

    Brew formula update for popeye version v0.3.4

--- Formula/popeye.rb
+++ Formula/popeye.rb
@@ -2,9 +2,9 @@
 class Popeye < Formula
   desc "A Kubernetes Cluster sanitizer and linter."
   homepage "https://imhotep.io/popeye"
-  url "https://github.com/derailed/popeye/releases/download/v0.3.3/popeye_0.3.3_Darwin_x86_64.tar.gz"
-  version "0.3.3"
-  sha256 "7de39b438c0932bd6b82e954751d0a034122f49624b9a935d031f6d777fe3b53"
+  url "https://github.com/derailed/popeye/releases/download/v0.3.4/popeye_0.3.4_Darwin_x86_64.tar.gz"
+  version "0.3.4"
+  sha256 "72fb4215b8a6848c9c157d0b2f58b81c62052e7f890214b6f85f384afa8910ce"
 
   def install
     bin.install "popeye"
